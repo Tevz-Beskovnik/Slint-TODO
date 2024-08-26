72d4365bde28d55160fdad99870db1ae7982cd52
tevzi2 <tevz.beskovnik@gmail.com>
2024-08-26 23:07:20 +0200

    Added button to add new lists

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::rc::Rc;
 
 use slint::{ComponentHandle, ModelRc, SharedString};
 use slint_test::io_handler::IoHandlerFs;
-use slint_test::io_handler_trait::{IoHandlerTrait, Task as TaskInternal};
+use slint_test::io_handler_trait::{
+    IoHandlerTrait, ListReduced as ListReducedInternal, Task as TaskInternal,
+};
 use slint_test::vec_to_model_rc;
 
 slint::include_modules!();
@@ -20,6 +22,18 @@ pub fn task_list_to_model_rc(input: Vec<TaskInternal>) -> ModelRc<Task> {
     )
 }
 
+pub fn reduced_lists_to_model_rc(input: Vec<ListReducedInternal>) -> ModelRc<ListReduced> {
+    vec_to_model_rc(
+        input
+            .into_iter()
+            .map(|x| ListReduced {
+                name: SharedString::from(&x.name),
+                id: x.id as i32,
+            })
+            .collect::<Vec<ListReduced>>(),
+    )
+}
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     let io_handler_rc = Rc::new(IoHandlerFs::new("./todos.json".to_string()));
@@ -32,6 +46,9 @@ fn main() -> Result<(), slint::PlatformError> {
     todo_logic.set_list_name(SharedString::from(&list.name));
     todo_logic.set_task_list(list_tasks);
     todo_logic.set_list_index(0);
+    todo_logic.set_lists(reduced_lists_to_model_rc(
+        io_handler_rc.get_all_reduced_lists(),
+    ));
 
     todo_logic.on_update_task({
         let io_handler_rc = io_handler_rc.clone();
@@ -93,6 +110,41 @@ fn main() -> Result<(), slint::PlatformError> {
             let _ = io_handler_rc.remove_task(task_id as usize);
             let list = io_handler_rc.get_active_list();
             weak_todo_logic.set_task_list(task_list_to_model_rc(list.tasks.clone()));
+
+            let _ = io_handler_rc.write_to_file();
+        }
+    });
+
+    todo_logic.on_add_list({
+        let weak_update = ui.as_weak();
+        let io_handler_rc = io_handler_rc.clone();
+        move || {
+            let weak_app = weak_update.unwrap();
+            let weak_todo_logic = weak_app.global::<TodoLogic>();
+            io_handler_rc.add_list();
+            weak_todo_logic.set_lists(reduced_lists_to_model_rc(
+                io_handler_rc.get_all_reduced_lists(),
+            ));
+
+            let _ = io_handler_rc.write_to_file();
+        }
+    });
+
+    todo_logic.on_remove_list({
+        let weak_update = ui.as_weak();
+        let io_handler_rc = io_handler_rc.clone();
+        move |list_id| {
+            let weak_app = weak_update.unwrap();
+            let weak_todo_logic = weak_app.global::<TodoLogic>();
+            let _ = io_handler_rc.remove_list(list_id as usize);
+            let _ = io_handler_rc.set_active_list(0);
+            weak_todo_logic.set_lists(reduced_lists_to_model_rc(
+                io_handler_rc.get_all_reduced_lists(),
+            ));
+            weak_todo_logic.set_list_index(0);
+            weak_todo_logic.set_lists(reduced_lists_to_model_rc(
+                io_handler_rc.get_all_reduced_lists(),
+            ));
 
             let _ = io_handler_rc.write_to_file();
         }
